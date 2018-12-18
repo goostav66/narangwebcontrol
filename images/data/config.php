e177f239c4977955bdd17d7815d44c0f7a8d89b1
goostav66 <34259724+goostav66@users.noreply.github.com>
2018-12-18 13:18:53 +0900

    Create config.php

---
+++ images/data/config.php
@@ -0,0 +1,9 @@
+<?php
+	define("DB_HOST", "61.250.89.75");
+	define("DB_USER", "hanjicds");
+	define("DB_PASSWORD", "gkwlakfk");
+	define("DB_NAME", "jnfc");
+
+	define("GOOGLE_API_KEY", "AAAA05pbp7g:APA91bHSZw05vhuPCoOUZrJBJDsODij72Wyr6TslDUq2b0wE6LlYJa7_TU7xRqsd3wuoEX-Y5P-1AkvUDQ3rCE9Si7XDSJvb1zylHe-ECKAhQQQhah5xhwwzOf4jvLdYXms4n4PDKp7TvdBqyNgchMgtGkBfq7B_9g"); 
+
+?>
